2c59c62d4401ec68d858a1bdc703c8d7eaf60ebe
pattyheiden <patriciasilvalimah@gmail.com>
2023-12-10 15:12:01 -0300

    api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export const api = axios.create({
   // baseURL: 'http://localhost:3000',
-  baseURL: 'https://api.hmshare.com.br',
+  baseURL: 'https://api.estudiopetbnu.com.br',
   headers: {
     accept: 'application/json',
     'content-type': 'application/json',
